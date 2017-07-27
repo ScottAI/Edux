ff1c1e14ff08e8bb1739f32f68124085252f2c34
Murat Demirci <mdemirci@outlook.com>
2017-07-27 17:34:18 +0300

    DataTable entities added

--- Edux/Migrations/20170727143331_addDataTable.Designer.cs
+++ Edux/Migrations/20170727143331_addDataTable.Designer.cs
@@ -9,9 +9,10 @@ using Edux.Models;
 namespace Edux.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170727143331_addDataTable")]
+    partial class addDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
