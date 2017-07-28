c1c1c8cc008163801c649cd08389d88a7f77939c
Murat Demirci <mdemirci@outlook.com>
2017-07-28 14:42:52 +0300

    form entities added

---
+++ Edux/Models/InputType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.Models
+{
+    public enum InputType
+    {
+        Text,
+        TextArea,
+        Html,
+        Select,
+        Checkbox            
+    }
+}
