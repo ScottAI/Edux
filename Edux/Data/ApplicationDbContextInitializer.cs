ae02cf6d6cdc057a29692f920c42e64a03c68fb5
Murat Demirci <mdemirci@outlook.com>
2017-08-01 18:10:53 +0300

    seed

--- Edux/Data/ApplicationDbContextInitializer.cs
+++ Edux/Data/ApplicationDbContextInitializer.cs
@@ -19,16 +19,52 @@ namespace Edux.Data
                 return;   // DB has been seeded
             }
 
-            var componentTypes = new ComponentType[]
-            {
-            new ComponentType{ Name="DivComponent", DisplayName="Div Bileşeni", CreateDate=DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy="username", UpdatedBy="username", AppTenantId="1"},
-            new ComponentType{ Name="LinkComponent", DisplayName="Link Bileşeni", CreateDate=DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy="username", UpdatedBy="username", AppTenantId="1"},
-            new ComponentType{ Name="DataTableComponent", DisplayName="Veri Tablosu Bileşeni", CreateDate=DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy="username", UpdatedBy="username", AppTenantId="1"}
-            };
-            foreach (ComponentType ct in componentTypes)
-            {
-                context.ComponentTypes.Add(ct);
-            }
+            // Div bileşenini ekle
+            var ct1 = new ComponentType() { Name = "DivComponent", DisplayName = "Div Bileşeni", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", AppTenantId = "1" };
+            context.ComponentTypes.Add(ct1);
+            context.SaveChanges();
+
+            // Div bileşeninin parametrelerini ekle
+            var p1 = new Parameter() { Name = "CssClass", DisplayName = "CssClass", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct1.Id };
+            context.Parameters.Add(p1);
+            context.SaveChanges();
+
+            // Link bileşenini ekle
+            var ct2 = new ComponentType() { Name = "LinkComponent", DisplayName = "Link Bileşeni", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", AppTenantId = "1" };
+            context.ComponentTypes.Add(ct2);
+            context.SaveChanges();
+
+            // link bileşeninin parametrelerini ekle
+            var p2 = new Parameter() { Name = "CssClass", DisplayName = "CssClass", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct2.Id };
+            var p3 = new Parameter() { Name = "Href", DisplayName = "Href", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct2.Id };
+            var p4 = new Parameter() { Name = "Text", DisplayName = "Text", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct2.Id };
+            context.Parameters.Add(p2);
+            context.Parameters.Add(p3);
+            context.Parameters.Add(p4);
+            context.SaveChanges();
+
+            // Image bileşenini ekle
+            var ct3 = new ComponentType() { Name = "ImageComponent", DisplayName = "Resim Bileşeni", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", AppTenantId = "1" };
+            context.ComponentTypes.Add(ct3);
+            context.SaveChanges();
+
+            // link bileşeninin parametrelerini ekle
+            var p5 = new Parameter() { Name = "Src", DisplayName = "Src", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct3.Id };
+            var p6 = new Parameter() { Name = "Width", DisplayName = "Width", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct3.Id };
+            var p7 = new Parameter() { Name = "Height", DisplayName = "Height", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct3.Id };
+            context.Parameters.Add(p5);
+            context.Parameters.Add(p6);
+            context.Parameters.Add(p7);
+            context.SaveChanges();
+
+            // veri tablosu bileşenini ekle
+            var ct4 = new ComponentType { Name = "DataTableComponent", DisplayName = "Veri Tablosu Bileşeni", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", AppTenantId = "1" };
+            context.ComponentTypes.Add(ct4);
+            context.SaveChanges();
+
+            // veri tablosu bileşeninin parametrelerini ekle
+            var p8 = new Parameter() { Name = "EntityName", DisplayName = "EntityName", CreateDate = DateTime.Parse("2017-07-26"), UpdateDate = DateTime.Parse("2017-07-26"), CreatedBy = "username", UpdatedBy = "username", ComponentTypeId = ct4.Id };
+            context.Parameters.Add(p8);
             context.SaveChanges();
         }
     }
