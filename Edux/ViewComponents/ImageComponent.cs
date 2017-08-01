375f463f2201b5945b6bbad9198cbf5e9b017c59
Ersin Mert Demircan <ersinmert_demircan@hotmail.com>
2017-08-01 17:45:49 +0300

    Pages Module's Bugs were edited.

---
+++ Edux/ViewComponents/ImageComponent.cs
@@ -0,0 +1,28 @@
+﻿using Edux.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.ViewComponents
+{
+    public class ImageComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public ImageComponent(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Models.Component component)
+        {
+            var viewName = component.View ?? "Default";
+            ViewBag.Src = component.ParameterValues.FirstOrDefault(f => f.Parameter.Name == "Src").Value;
+            ViewBag.Width= component.ParameterValues.FirstOrDefault(f => f.Parameter.Name == "Width").Value;
+            ViewBag.Height = component.ParameterValues.FirstOrDefault(f => f.Parameter.Name == "Height").Value;
+
+            return await Task.FromResult(View(viewName, component));
+        }
+    }
+}
