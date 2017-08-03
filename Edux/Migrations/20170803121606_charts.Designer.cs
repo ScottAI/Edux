ddee0186227d412bf6f3e9da4140ba4f210499bd
ayse özbek <ayseozbek6144@outlook.com>
2017-08-03 15:30:23 +0300

    Chart ve ChartType Eklendi

--- Edux/Migrations/20170803121606_charts.Designer.cs
+++ Edux/Migrations/20170803121606_charts.Designer.cs
@@ -9,9 +9,10 @@ using Edux.Models;
 namespace Edux.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170803121606_charts")]
+    partial class charts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
