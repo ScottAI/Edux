fffd3426bb37d916ce6d573a5df5f0d81d4c7601
Murat Demirci <mdemirci@outlook.com>
2017-09-07 13:03:01 +0300

    some form bugs fixed

--- Edux/ViewComponents/FormComponent.cs
+++ Edux/ViewComponents/FormComponent.cs
@@ -21,15 +21,15 @@ namespace Edux.ViewComponents
         {
             var viewName = component.View ?? "Default";
             var formName = component.ParameterValues.FirstOrDefault(f => f.Parameter.Name == "FormName")?.Value;
-            var mode = component.ParameterValues.FirstOrDefault(f => f.Parameter.Name == "Mode")?.Value;
+            string mode = Request.Query["mode"].ToString();
             ViewBag.Mode = mode;
             var rowId = Request.Query["id"].ToString();
-            rowId = ViewBag.RowId;
+            ViewBag.RowId = rowId;
             ViewBag.Form = await _context.Forms.Include(f => f.Fields).ThenInclude(ff => ff.Property).ThenInclude(p => p.PropertyValues).SingleOrDefaultAsync(f => f.Name == formName);
             var entityName = ((Form)ViewBag.Form).EntityName;
-            if (mode == "Edit" && !String.IsNullOrEmpty(ViewBag.RowId))
+            if (mode.ToLowerInvariant() == "edit" && !String.IsNullOrEmpty(ViewBag.RowId))
             {
-                ViewBag.RowValues = await _context.PropertyValues.Include(i => i.Entity).Include(i => i.Property).Where(p => p.Entity.Name == entityName && p.RowId == Convert.ToInt64(rowId)).OrderBy(r => r.RowId).FirstOrDefaultAsync();
+                ViewBag.RowValues = await _context.PropertyValues.Include(i => i.Entity).Include(i => i.Property).Where(p => p.Entity.Name == entityName && p.RowId == Convert.ToInt64(rowId)).OrderBy(r => r.RowId).ToListAsync();
             }
             return await Task.FromResult(View(viewName, component));
 
