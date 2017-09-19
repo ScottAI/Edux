e8bf7aed20fc8247d18cba6474d4df33563f00e2
Murat Demirci <mdemirci@outlook.com>
2017-09-19 10:23:18 +0300

    select bug fixed

--- Edux/ViewComponents/DataTableComponent.cs
+++ Edux/ViewComponents/DataTableComponent.cs
@@ -36,9 +36,9 @@ namespace Edux.ViewComponents
             var datatable = await _context.DataTables.Include(e => e.Columns).ThenInclude(e => e.Property).ThenInclude(pv => pv.PropertyValues).SingleOrDefaultAsync(e => e.Name == DataTableName);
             ViewBag.DataTable = datatable;
             var entityName = datatable.EntityName;
-            var values = await _context.PropertyValues.Include(i => i.Entity).Include(i => i.Property).Where(x => x.Entity.Name == entityName && _context.PropertyValues.Where(
+            var values = _context.PropertyValues.Include(i => i.Entity).Include(i => i.Property).ThenInclude(t => t.DataSourceProperty).ThenInclude(v=>v.PropertyValues).Where(x => x.Entity.Name == entityName && _context.PropertyValues.Where(
                 p => (datatable.Columns.Any(c=>c.FilterOperator != Models.FilterOperator.None)?(datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterOperator == Models.FilterOperator.Equals ?
-                    (p.Value == datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterValue) : (datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterOperator == Models.FilterOperator.Contains ? (p.Value.Contains(datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterValue)) : false)):true)).Any(f=>f.RowId==x.RowId)).OrderBy(r => r.RowId).Take(datatable.Top).ToListAsync();
+                    (p.Value == datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterValue) : (datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterOperator == Models.FilterOperator.Contains ? (p.Value.Contains(datatable.Columns.FirstOrDefault(c => c.PropertyId == p.PropertyId).FilterValue)) : false)):true)).Any(f=>f.RowId==x.RowId)).OrderBy(r => r.RowId).Take(datatable.Top).ToList();
            
 
 
