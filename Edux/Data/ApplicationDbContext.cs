babb055b03d8791736c7a422ce5e9f44a845d8b6
Arzu Gedik <arzugedik312@gmail.com>
2017-09-21 10:24:33 +0300

    added multi-tenancy

--- Edux/Data/ApplicationDbContext.cs
+++ Edux/Data/ApplicationDbContext.cs
@@ -6,32 +6,54 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Edux.Models;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.AspNetCore.Http;
 
 namespace Edux.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, string>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Role, string>
         {
-        }
-        public DbSet<Entity> Entities { get; set; }
-        public DbSet<Property> Properties { get; set; }
-        public DbSet<PropertyValue> PropertyValues { get; set; }
-        public DbSet<ComponentType> ComponentTypes { get; set; }
-        public DbSet<Page> Pages { get; set; }
-        public DbSet<ParameterValue> ParameterValues { get; set; }
-        public DbSet<Parameter> Parameters { get; set; }
-        public DbSet<Component> Components { get; set; }
-        public DbSet<DataTable> DataTables { get; set; }
-        public DbSet<Column> Columns { get; set; }
-        public DbSet<Form> Forms { get; set; }
-        public DbSet<Field> Fields { get; set; }
-        public DbSet<Menu> Menus { get; set; }
-        public DbSet<MenuItem> MenuItems { get; set; }
-        public DbSet<Chart> Charts { get; set; }
-        public DbSet<Setting> Settings { get; set; }
-        public DbSet<App> Apps{ get; set; }
+            private readonly AppTenant tenant;
+            private readonly IHttpContextAccessor _accessor;
+            public ApplicationDbContext() { }
+            public ApplicationDbContext(AppTenant tenant, IHttpContextAccessor accessor)
+                {
+                 _accessor = accessor;
+
+                    if (tenant != null)
+                        {
+                         this.tenant = tenant;
+                         var tenantId = this.tenant.AppTenantId;
+                         this.Initialize(accessor);
+                         ApplicationDbContextInitializer.Initialize(this, accessor);
+
+                //QueryFilterManager.Filter<Page>(q => q.Where(x => x.AppTenantId == tenantId));
+
+                //QueryFilterManager.InitilizeGlobalFilter(this);
+            }
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer((tenant != null ? tenant.ConnectionString : "Server=.;Database=TenantDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+                base.OnConfiguring(optionsBuilder);
+            }
+            public DbSet<Entity> Entities { get; set; }
+            public DbSet<Property> Properties { get; set; }
+            public DbSet<PropertyValue> PropertyValues { get; set; }
+            public DbSet<ComponentType> ComponentTypes { get; set; }
+            public DbSet<Page> Pages { get; set; }
+            public DbSet<ParameterValue> ParameterValues { get; set; }
+            public DbSet<Parameter> Parameters { get; set; }
+            public DbSet<Component> Components { get; set; }
+            public DbSet<DataTable> DataTables { get; set; }
+            public DbSet<Column> Columns { get; set; }
+            public DbSet<Form> Forms { get; set; }
+            public DbSet<Field> Fields { get; set; }
+            public DbSet<Menu> Menus { get; set; }
+            public DbSet<MenuItem> MenuItems { get; set; }
+            public DbSet<Chart> Charts { get; set; }
+            public DbSet<Setting> Settings { get; set; }
+            public DbSet<App> Apps{ get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ Edux/Models/Theme.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.Models
+{
+    public class Theme
+    {
+        public long Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+
+        [Display(Name = "Açıklama")]
+        public string Description { get; set; }
+
+        [Display(Name = "Resim Yolu")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Sayfa Şablonları")]
+        public string PageTemplates { get; set; }
+
+        [Display(Name = "Bileşen Şablonları")]
+        public string ComponentTemplates { get; set; }
+
+        public string Logo { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Meta Başlık")]
+        public string MetaTitle { get; set; }
+
+        [Display(Name = "Meta Açıklama")]
+        public string MetaDescription { get; set; }
+        [Display(Name = "Menu Konumları")]
+        public string MenuLocations { get; set; }
+        [Display(Name = "Anahtar kelimeler")]
+        public string MetaKeywords { get; set; }
+        public string CustomCSS { get; set; }
+        [Display(Name = "Çok lokasyonluluk")]
+        public string ManyLocation { get; set; }
+        [Display(Name = "Oluşturulma Tarihi")]
+        public DateTime CreateDate { get; set; }
+        [StringLength(200)]
+        [Required]
+        [Display(Name = "Oluşturan Kişi")]
+        public string CreatedBy { get; set; }
+        [Display(Name = "Güncelleme Tarihi")]
+        public DateTime UpdateDate { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Güncelleyen Kişi")]
+        public string UpdatedBy { get; set; }
+    }
+}
