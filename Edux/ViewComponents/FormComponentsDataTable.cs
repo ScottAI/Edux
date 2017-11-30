a442c32a8aec5a8266527e892e2130dc4e764242
ezgisahin <ezgisahin87@gmail.com>
2017-11-30 14:15:08 +0300

    add FormComponentsDataTable

---
+++ Edux/ViewComponents/FormComponentsDataTable.cs
@@ -0,0 +1,26 @@
+﻿using Edux.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.ViewComponents
+{
+    public class FormComponentsDataTable : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public FormComponentsDataTable(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string formId)
+        {
+            var components = _context.Components.Include(c => c.ComponentType).Include(c => c.ParentComponent).Include(c => c.Page).Where(c => c.FormId == formId).OrderBy(o => o.Position);
+            ViewBag.FormId = formId;
+            return View(await components.ToListAsync());
+        }
+    }
+}
