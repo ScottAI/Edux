1033c6fde43e0a5cb900b2c33987582a9e0c28a0
Murat Demirci <mdemirci@outlook.com>
2017-12-26 11:22:11 +0300

    saveform now works

---
+++ Edux/Migrations/20171226064705_migFix.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Edux.Migrations
+{
+    public partial class migFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ComponentId",
+                table: "Fields",
+                type: "nvarchar(450)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ComponentId",
+                table: "Fields",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
